0e301d295dff740155b546196c55664148d4fe27
neelap <neela_reddy@outlook.com>
2016-05-10 07:04:27 +0530

    monitoring

---
+++ src/test/java/com/nems/dashboard/NemsTurbineApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nems.dashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = NemsTurbineApplication.class)
+public class NemsTurbineApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
